4c8581fb9ba445122632b7be54ee4133e062e36a
Andrew McKelvy <ajm0002@uah.edu>
2019-09-13 07:57:29 -0500

    Switched the Java Maven proj out for this Java Ant proj. SQLite is already plugged into the included class path. Example code is in the main.

---
+++ src/cs499_ophthalmology_emr/CS499_Ophthalmology_EMR.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs499_ophthalmology_emr;
+
+/**
+ *
+ * @author TheWitchDoctor
+ */
+
+//public class CS499_Ophthalmology_EMR {
+
+    /**
+     * @param args the command line arguments
+     */
+ //   public static void main(String[] args) {
+        // TODO code application logic here
+ //   }
+    
+//}
+
+import java.sql.*;
+// This is only to test JDBC. This should not be coded into.
+public class CS499_Ophthalmology_EMR {      
+  public static void main( String args[] ) {
+      Connection c = null;
+      
+      try {
+         Class.forName("org.sqlite.JDBC");
+         c = DriverManager.getConnection("jdbc:sqlite:test.db");
+      } catch ( Exception e ) {
+         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+         System.exit(0);
+      }
+      System.out.println("Opened database successfully");
+   }
+}
