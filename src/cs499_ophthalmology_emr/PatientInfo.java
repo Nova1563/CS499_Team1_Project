688fc6c0208f08b04fc7a260df19cdd7946c63c7
Ryan_Davis <36085749+RDavisss@users.noreply.github.com>
2019-10-12 02:11:13 -0400

    Finished getters and setters for patientInfo.java

--- src/cs499_ophthalmology_emr/PatientInfo.java
+++ src/cs499_ophthalmology_emr/PatientInfo.java
@@ -18,6 +18,22 @@ public class PatientInfo
     private int age;
     private int zip;
     private int patientID;
+    private int homePhone;
+    private int workPhone;
+    private int mobilePhone;
+    private String gender;
+    private int dateOfBirth;
+    private String emailAddress;
+    private int ssn;
+    private int emgergContactNum;
+    private String emergContactName;
+    private String insProvider;
+    private int insContactNo;
+    private int insGroupNo;
+    private int insEffectiveDate;
+    private float insCoPayAmount;
+    private String insProviderAddr;
+    private int insProviderPhone;
     
     List<PatientAppointment> appointmentList = new ArrayList<>();
     
@@ -69,4 +85,216 @@ public class PatientInfo
         this.patientID = patientID;
     }
     //-------------------------------
+
+    
+    //-------------------------------
+    public int getHomePhone() 
+    {
+        return homePhone;
+    }
+
+    public void setHomePhone(int homePhone) 
+    {
+        this.homePhone = homePhone;
+    }
+    //-------------------------------
+    
+    
+    //-------------------------------
+    public int getWorkPhone() 
+    {
+        return workPhone;
+    }
+    
+    public void setWorkPhone(int workPhone) 
+    {
+        this.workPhone = workPhone;
+    }
+    //-------------------------------
+    
+    
+    //-------------------------------
+    public int getMobilePhone() 
+    {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(int mobilePhone) 
+    {
+        this.mobilePhone = mobilePhone;
+    }
+    //-------------------------------
+    
+
+    //-------------------------------
+    public String getGender() 
+    {
+        return gender;
+    }
+    
+    public void setGender(String gender) 
+    {
+        this.gender = gender;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public int getDateOfBirth() 
+    {
+        return dateOfBirth;
+    }
+    
+    public void setDateOfBirth(int dateOfBirth) 
+    {
+        this.dateOfBirth = dateOfBirth;
+    }
+    //-------------------------------
+    
+    
+    //-------------------------------
+    public String getEmailAddress() 
+    {
+        return emailAddress;
+    }
+    
+    public void setEmailAddress(String emailAddress) 
+    {
+        this.emailAddress = emailAddress;
+    }
+    //-------------------------------
+    
+    
+    //-------------------------------
+    public int getSsn() 
+    {
+        return ssn;
+    }
+    
+    public void setSsn(int ssn) 
+    {
+        this.ssn = ssn;
+    }
+    //-------------------------------
+    
+    
+    //-------------------------------
+    public int getEmgergContactNum() 
+    {
+        return emgergContactNum;
+    }
+    
+     public void setEmgergContactNum(int emgergContactNum) 
+    {
+        this.emgergContactNum = emgergContactNum;
+    }
+    //-------------------------------
+     
+     
+    //-------------------------------
+    public String getEmergContactName() 
+    {
+        return emergContactName;
+    }
+
+    public void setEmergContactName(String emergContactName) 
+    {
+        this.emergContactName = emergContactName;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public String getInsProvider() 
+    {
+        return insProvider;
+    }
+
+    public void setInsProvider(String insProvider) 
+    {
+        this.insProvider = insProvider;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public int getInsContactNo() 
+    {
+        return insContactNo;
+    }
+
+    public void setInsContactNo(int insContactNo) 
+    {
+        this.insContactNo = insContactNo;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public int getInsGroupNo() 
+    {
+        return insGroupNo;
+    }
+
+    public void setInsGroupNo(int insGroupNo) 
+    {
+        this.insGroupNo = insGroupNo;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public int getInsEffectiveDate() 
+    {
+        return insEffectiveDate;
+    }
+
+    public void setInsEffectiveDate(int insEffectiveDate) 
+    {
+        this.insEffectiveDate = insEffectiveDate;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public float getInsCoPayAmount() 
+    {
+        return insCoPayAmount;
+    }
+
+    public void setInsCoPayAmount(float insCoPayAmount) 
+    {
+        this.insCoPayAmount = insCoPayAmount;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public String getInsProviderAddr() 
+    {
+        return insProviderAddr;
+    }
+
+    public void setInsProviderAddr(String insProviderAddr) 
+    {
+        this.insProviderAddr = insProviderAddr;
+    }
+    //-------------------------------
+
+    
+    //-------------------------------
+    public int getInsProviderPhone() 
+    {
+        return insProviderPhone;
+    }
+
+    public void setInsProviderPhone(int insProviderPhone) 
+    {
+        this.insProviderPhone = insProviderPhone;
+    }
+    //-------------------------------
+    
+    
+    
+   
 }
